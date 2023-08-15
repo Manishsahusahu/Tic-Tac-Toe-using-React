01c7a76fc4b58ecb212f2bc2ad18034ea61552e2
Manishsahusahu <sahumanish327@gmail.com>
2023-08-15 17:16:24 +0530

    logic of game

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,9 +2,9 @@ import Icon from "../Icon/Icon";
 import React from "react";
 import "./card.css";
 
-const Card = ({ player }) => {
+const Card = ({ player, onPlay, index }) => {
   return (
-    <div className="card">
+    <div className="card" onClick={() => onPlay(index)}>
       <Icon name={player == "X" ? "cross" : player == "O" ? "circle" : ""} />
     </div>
   );
--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,14 +1,48 @@
 import React, { useState } from "react";
 import "./Grid.css";
 import Card from "../Card/Card";
+import { isWinner } from "../helpers/checkWinner";
 
 const Grid = ({ numberOfCards }) => {
   const [board, setBoard] = useState(Array(numberOfCards).fill(""));
+  const [turn, setTurn] = useState(true);
+  const [winner, setWinner] = useState(null);
+
+  function play(index) {
+    if (turn === true) {
+      board[index] = "O";
+    } else {
+      board[index] = "X";
+    }
+    const win = isWinner(board, turn ? "O" : "X");
+    if (win) setWinner(win);
+
+    setBoard([...board]);
+    setTurn(!turn);
+  }
+
+  function reset() {
+    setTurn(true);
+    setBoard(Array(numberOfCards).fill(""));
+    setWinner(null);
+  }
+
   return (
-    <div className="grid">
-      {board.map((el, index) => (
-        <Card key={index} />
-      ))}
+    <div className="grid-wrapper">
+      {winner && (
+        <>
+          <h1 className="turn-highlight">Winner is {winner}</h1>
+          <button className="reset" onClick={reset}>
+            Reset Game
+          </button>
+        </>
+      )}
+      <h1 className="turn-highlight">Current turn: {turn ? "O" : "X"}</h1>
+      <div className="grid">
+        {board.map((el, index) => (
+          <Card key={index} onPlay={play} player={el} index={index} />
+        ))}
+      </div>
     </div>
   );
 };
